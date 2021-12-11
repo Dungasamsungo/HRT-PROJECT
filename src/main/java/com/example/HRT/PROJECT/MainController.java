053fddd472305b7b074c2cd37042def8093485dc
sampson <sampsonngobi@gmail.com>
2021-12-11 18:44:11 -0300

    Update and Delete methods add.

--- src/main/java/com/example/HRT/PROJECT/MainController.java
+++ src/main/java/com/example/HRT/PROJECT/MainController.java
@@ -2,8 +2,11 @@ package com.example.HRT.PROJECT;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -41,4 +44,38 @@ public class MainController {
           public @ResponseBody Iterable<User>getAllUsers(){
               return uRepository.findAll();
           }
+
+
+    @PutMapping (path= "/update/{id}")
+
+          public @ResponseBody String uddateUser(
+                @PathVariable int id, 
+                @RequestParam String name,
+                @RequestParam String status,
+                @RequestParam String location,
+                @RequestParam Integer proposedStartTime,
+                @RequestParam Integer actualStartTime,
+                @RequestParam Integer surgeryEndTime,
+                @RequestParam Integer exitTime
+              ){
+                User u = uRepository.findById(id);
+                u.setName(name);
+                u.setStatus(status);
+                u.setLocation(location);
+                u.setProposedStartTime(proposedStartTime);
+                u.setActualStartTime(actualStartTime);
+                u.setSurgeryEndTime(surgeryEndTime);
+                u.setExitTime(exitTime);
+                uRepository.save(u);
+
+            return ("Patient Updated");
+          }
+
+    @DeleteMapping("/delete/{id}")
+
+          public @ResponseBody String deleteUser(@PathVariable int id){
+                uRepository.deleteById(id);
+            return "Patient Deleted";
+          }
+          
 }
